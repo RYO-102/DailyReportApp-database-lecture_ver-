09530023d97e3cfcf220b93c34f2a716aa01984f
RYO-102 <ryou_ryou@docomo.ne.jp>
2025-12-23 23:54:02 +0900

    forms.pyのリファクタリング 不要なコメントを削除。

--- reports/forms.py
+++ reports/forms.py
@@ -4,13 +4,11 @@ from .models import DailyReport
 class DailyReportForm(forms.ModelForm):
     class Meta:
         model = DailyReport
-        # フォームに入力させるフィールドを指定
         fields = ['category', 'tags', 'condition', 'title', 'content', 'image']
         
-        # 見た目を整えるためのウィジェット設定（Bootstrap風のクラス付与など）
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '今日のタイトル'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': '業務内容や雑談など...'}),
-            'tags': forms.CheckboxSelectMultiple(), # タグを複数チェックボックスで選ばせる
-            'condition': forms.RadioSelect(),       # 調子はラジオボタンで選ばせる
+            'tags': forms.CheckboxSelectMultiple(),
+            'condition': forms.RadioSelect(),
         }
